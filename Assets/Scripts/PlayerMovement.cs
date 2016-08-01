07a26f1bbdd05fc3dd48b717892c05b62ce78103
CawawaC <cawawac@gmail.com>
2016-08-01 15:36:22 +0200

    Déplacement grossier v2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public BoxCollider boundingBox;
+
+
+	// Use this for initialization
+	void Start () {
+
+  }
+
+	// Update is called once per frame
+	void Update () {
+        float movey = new Vector3(0, Input.GetAxis("Vertical"), 0) * speed;
+        float movex = new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed;
+
+        if(boundingBox.bounds.Contains(transform.position + movex))
+          transform.position += movex;
+
+        if(boundingBox.bounds.Contains(transform.position + movey))
+          transform.position += movey;
+    }
+}
